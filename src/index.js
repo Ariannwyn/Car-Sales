b6f7291317ca9f8c120fbf58fe1b41e300ef2c9c
EmilyA <emilyadams.3d@gmail.com>
2020-09-08 16:01:30 -0700

    Work in progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { reducer, initialState } from "./reducers/reducer";
+import { createStore } from "redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(() => initialState);
+console.log("index store", store);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
